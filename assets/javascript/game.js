aa3186cd7bed5b7ddddb56d19ad6c3755d719fd1
Wes Van Duine <Wes Van Duine>
2017-05-05 20:28:59 -0700

    initial html

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+/*==============VARIABLES=====================================*/
+
+/*Crystals*/
+var crystalGreen=0;
+var crystalPurple=0;
+var crystalBlue=0;
+var crystalYellow= 0;
+
+/*Scores (current & target)*/
+var targetScore=0;
+var currentScore=0;
+
+
+/*Wins & Loses*/
+var wins=0;
+var losses=0;
+
+
+
+/*===============FUNCTIONS==============*/
+
+
+
+
+
+
+
+/*===============GAME LOGIC================*/
+$("#green").click(function(){
+	alert("test");
+})
+
+$("#purple").click(function(){
+	alert("test");
+})
+
+$("#blue").click(function(){
+	alert("test");
+})
+
+$("#yellow").click(function(){
+	alert("test");
+})
