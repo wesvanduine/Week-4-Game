43fa6e0a009c7b8a401de86fd2f32dd6dddb8309
Wes Van Duine <Wes Van Duine>
2017-05-05 23:13:46 -0700

    adding more functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 /*==============VARIABLES=====================================*/
 
 /*Crystals*/
+
 var crystalGreen= 0;
 var crystalPurple= 0;
 var crystalBlue= 0;
@@ -20,7 +21,7 @@ var losses= 0;
 /*===============FUNCTIONS==============*/
 function startGame(){
 	// reset current score to 0
-	var currentScore = 0;
+	currentScore = 0;
 
 	// set target score to random number between 19-120
 	targetScore = Math.floor(Math.random() * (120-19 + 1)) + 19; 
@@ -48,29 +49,66 @@ function startGame(){
 
 	//when crystal is clicked add value 
 	function addGreen(){
-		currentScore = crystalGreen += currentScore;
+		currentScore = crystalGreen + currentScore;
 		console.log(currentScore);
+		//change the html to refelect the current score
+		$("#currentScore").html(currentScore);
 	}
 	function addPurple(){
-		currentScore = crystalPurple += currentScore;
+		currentScore = crystalPurple + currentScore;
 		console.log(currentScore);
+		//change the html to refelect the current score
+		$("#currentScore").html(currentScore);
 	}
 	function addBlue(){
-		currentScore = crystalBlue += currentScore;
+		currentScore = crystalBlue + currentScore;
 		console.log(currentScore);
+		//change the html to refelect the current score
+		$("#currentScore").html(currentScore);
 	}
 	function addYellow(){
-		currentScore = crystalYellow += currentScore;
+		currentScore = crystalYellow + currentScore;
 		console.log(currentScore);
+		//change the html to refelect the current score
+		$("#currentScore").html(currentScore);
+
+		//run the checkWin Function
+		checkWin();
+	}
+
+	//check if user won or lost and reset the game
+	function checkWin(){
+		
+		if (currentScore > targetScore) {
+			alert("You lost!");
+			losses ++;
+
+			//change html to reflect losses
+			$("#lossCount").html(losses);
+
+			//restarts the game
+			startGame();
+		}
+
+		else if (currentScore === targetScore){
+			alert("You Won!");
+			wins ++;
+
+			//change html to reflect losses
+			$("#winCount").html(wins);
+
+			//restarts the game
+			startGame();
+
+
+		}
 	}
 	
 	
-
-		//Testing console
-
-		
-
 /*===============GAME LOGIC================*/
+//Starts Game
+startGame();
+
 $("#green").click(function(){
 	addGreen();
 
@@ -88,5 +126,3 @@ $("#yellow").click(function(){
 	addYellow();
 })
 
-//Starts Game
-startGame();
